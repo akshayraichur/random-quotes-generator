e0e8dda5590445f26e847af7689fd92435af07dc
Akshay Raichur <lordakshay99@gmail.com>
2022-05-21 16:47:52 +0530

    fix: webpack config change

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
+  mode: "development",
   module: {
     rules: [
       {
@@ -10,19 +13,29 @@ module.exports = {
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
+        test: /\.js?$/,
+        use: ["babel-loader"],
+      },
+      {
         test: /\.html?$/,
         use: "raw-loader",
       },
     ],
+  },
+  resolve: {
+    extensions: [".ts", ".js", ".jsx"],
   },
   plugins: [
     new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "./src/index.html"),
     }),
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
   ],
   devServer: {
     static: path.join(__dirname, "dist", "index.html"),
+    open: true,
     compress: true,
     hot: true,
     port: 3000,
